22351b5c2407de82ca2633e995ee6263ceaa5cf5
unknown <mscc@cin.ufpe.br>
2017-10-03 22:24:31 -0300

    Passos 1 e 2 feitos

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
@@ -1,5 +1,8 @@
 package br.ufpe.cin.if710.podcast.domain;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -114,9 +117,9 @@ public class XmlFeedParser {
     // Processa tags do tipo <enclosure> para obter dados do episodio
     public static String readEnclosure(XmlPullParser parser)
             throws IOException, XmlPullParserException {
-        //parser.require(XmlPullParser.START_TAG, null, "enclosure");
-        String data = "implementar...";
-        //parser.require(XmlPullParser.END_TAG, null, "enclosure");
+        parser.require(XmlPullParser.START_TAG, null, "enclosure");
+        String data = parser.getAttributeValue(null,"url"); //basta pegar um atributo da tag, sem precisar usar os métodos da classe XmlFeedParser
+        //como enclosure é uma tag self-closing, não precisa chamar parser.require para END_TAG
         return data;
     }
 
